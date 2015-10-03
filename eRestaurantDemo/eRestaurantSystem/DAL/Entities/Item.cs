1f940246d0e59076b043fb2cf2ccaa282c9cbd7f
Junyu Yan <jyan6@studentmail.nait.ca>
2015-10-02 19:32:48 -0600

    fix entities

--- eRestaurantDemo/eRestaurantSystem/DAL/Entities/Item.cs
+++ eRestaurantDemo/eRestaurantSystem/DAL/Entities/Item.cs
@@ -32,6 +32,7 @@ namespace eRestaurantSystem.DAL.Entities
         public int MenuCategoryID { get; set; }
 
         public virtual MenuCategory Category { get; set; }
+        public virtual ICollection<BillItem> Billitems { get; set; }
 
         public Item()
         {
