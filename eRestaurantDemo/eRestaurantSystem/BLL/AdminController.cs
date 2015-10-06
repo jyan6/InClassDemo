da396bb8c747ad3b63bd931611fec226f3ada603
Junyu Yan <jyan6@studentmail.nait.ca>
2015-10-05 21:04:23 -0600

    repeater display

--- eRestaurantDemo/eRestaurantSystem/BLL/AdminController.cs
+++ eRestaurantDemo/eRestaurantSystem/BLL/AdminController.cs
@@ -81,5 +81,29 @@ namespace eRestaurantSystem.BLL
               return result.ToList();
           }
         }
+
+      [DataObjectMethod(DataObjectMethodType.Select, false)]
+      public List<MenuCategoryItems> MenuCategoryItems_List()
+      {
+          using (var context = new eRestaurantContext())
+          {
+              var result = from menuitem in context.MenuCategories
+                           orderby menuitem.Description
+                           select new MenuCategoryItems()
+                           {
+                               Description = menuitem.Description,
+                               MenuItems = from row in menuitem.MenuItems
+                                            select new MenuItem()
+                                              {
+                                                  Description = row.Description,
+                                                  Price = row.CurrentPrice,
+                                                  Calories = row.Calories,
+                                                  Comment = row.Comment
+                                              }
+                           };
+              return result.ToList();
+          }
+      }
+
     }
 }
